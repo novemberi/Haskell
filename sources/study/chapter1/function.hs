8b14e3e45d557ee44522f9a0091f08d0a192bbef
wolfi <contact@wolfi.in>
2020-08-22 17:50:26 +0900

    function

---
+++ sources/study/chapter1/function.hs
@@ -0,0 +1,14 @@
+-- f(1) = A
+-- f(2) = B
+-- f(3) = C
+-- {1,2,3} is {A,B,C}. This is valid function.
+
+-- f(1) = X
+-- f(1) = Y
+-- f(2) = Z
+-- This is not a valid function.
+
+-- f(1) = A
+-- f(2) = A
+-- f(3) = A
+-- This is valid function. Having the same output for more than one input is valid.
