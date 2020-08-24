0f6aead5e364722c9a4865dfc445c819c9189086
wolfi <contact@wolfi.in>
2020-08-24 20:43:52 +0900

    lambda

--- sources/study/chapter1/lambda.hs
+++ sources/study/chapter1/lambda.hs
@@ -1,8 +1,13 @@
--- λ x.x
+-- λx.x
 -- ^--^-------- extend of the head of the lambda.
 
--- λ x.x
+-- λx.x
 -- ^----------- the single parameter of the function. This binds any variables with the same name in the body of the function.
 
--- λ x.x
+-- λx.x
 --     ^------- body, the expression the lambda returns when applied. This is a bound variables.
+
+-- λx.x
+-- λy.y
+-- λz.z
+-- All the same function.
