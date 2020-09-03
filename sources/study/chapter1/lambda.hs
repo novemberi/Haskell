ce49c93e86c8aef9f6034e1c1c2525fb36331044
wolfi <contact@wolfi.in>
2020-09-03 20:16:30 +0900

    lambda

--- sources/study/chapter1/lambda.hs
+++ sources/study/chapter1/lambda.hs
@@ -1,16 +1,16 @@
--- λ𝑥.𝑥
+-- 𝜆𝑥.𝑥
 -- ^-^-------- extend of the head of the lambda.
 
--- λ𝑥.𝑥
+-- 𝜆𝑥.𝑥
 --  ^--------- the single parameter of the function. This binds any variables with the same name in the body of the function.
 
--- λ𝑥.𝑥
+-- 𝜆𝑥.𝑥
 --    ^------- body, the expression the lambda returns when applied. This is a bound variables.
 
--- λ𝑥.𝑥
--- λ𝑦.𝑦
--- λ𝑧.𝑧
+-- 𝜆𝑥.𝑥
+-- 𝜆𝑦.𝑦
+-- 𝜆𝑧.𝑧
 -- All the same function.
 
--- λ𝑥.𝑥𝑦
+-- 𝜆𝑥.𝑥𝑦
 --     ^ This is a free variables. The body expression has variables that are not named in the head.
