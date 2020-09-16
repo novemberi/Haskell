35a7d6cac2cdeba1066b22b3b894a0f58a0f38e5
wolfi <contact@wolfi.in>
2020-09-16 19:27:53 +0900

    hello

--- sources/study/chapter2/hello.hs
+++ sources/study/chapter2/hello.hs
@@ -6,3 +6,7 @@ sayHello x = putStrLn("Hello, " ++ x ++ "!")
 -- area 5 * 3 == (area 5) * 3
 -- area (5 * 3) == area 15
 area r = pi * r ^ 2
+double x = 2 * x
+quadruple x = double(double x)
+square x = x * x
+half x = x / 2
